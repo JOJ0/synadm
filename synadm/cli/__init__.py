0df92e14fc528a1fdcff5656d036983406b6c022
kaiyou <pierre@jaury.eu>
2020-12-12 17:31:10 +0100

    Refactor configuration handling

--- synadm/cli/__init__.py
+++ synadm/cli/__init__.py
@@ -29,7 +29,7 @@ def humanize(data):
         return tabulate(data.items(), tablefmt="simple")
 
 
-class APIClient(api.Synapse_admin):
+class APIClient:
     """ API client enriched with CLI-level functions
     """
 
@@ -41,80 +41,75 @@ class APIClient(api.Synapse_admin):
     }
 
     CONFIG = {
-        "user": None,
-        "token": None,
-        "base_url": None,
+        "user": "",
+        "token": "",
+        "base_url": "",
         "admin_path": "/_synapse/admin",
     }
 
-    def __init__(self, config_path, format):
+    def __init__(self, config_path, verbose, format):
         self.config = APIClient.CONFIG.copy()
         self.config_path = os.path.expanduser(config_path)
-        self.read_config()
         self.format = format
-        super(APIClient, self).__init__(log,
-            self.config["user"], self.config["token"],
-            self.config["base_url"], self.config["admin_path"])
+        self.log = self.init_logger(verbose)
+        self.api = None
+
+    def __getattr__(self, name):
+        print(name)
+        return getattr(self.api, name)          
+
+    def init_logger(self, verbose):
+        log_path = os.path.expanduser("~/.local/share/synadm/debug.log")
+        os.makedirs(os.path.dirname(log_path), exist_ok=True)
+        log = logging.getLogger('synadm')
+        log.setLevel(logging.DEBUG) # level of logger itself
+        f_handle = logging.FileHandler(log_path, encoding='utf-8') # create file handler
+        f_handle.setLevel(logging.DEBUG) # which logs even debug messages
+        c_handle = logging.StreamHandler() # console handler with a higher log level
+        c_handle.setLevel(
+            logging.DEBUG if verbose > 1 else
+            logging.INFO if verbose == 1 else
+            logging.WARNING
+        )
+        # create formatters and add it to the handlers
+        f_form = logging.Formatter('%(asctime)s %(name)-8s %(levelname)-7s %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S')
+        c_form = logging.Formatter('%(levelname)-5s %(message)s')
+        c_handle.setFormatter(c_form)
+        f_handle.setFormatter(f_form)
+        log.addHandler(c_handle) # add the handlers to logger
+        log.addHandler(f_handle)
+        return log
 
     def read_config(self):
         try:
             with open(self.config_path) as handle:
                 self.config.update(yaml.load(handle, Loader=yaml.SafeLoader))
-            log.debug("configuration read: {}".format(self.config))
+            self.log.debug("configuration read: {}".format(self.config))
         except Exception as error:
-            log.error("{} while reading configuration file".format(error))
+            self.log.error("{} while reading configuration file".format(error))
         for key, value in self.config.items():
-            if value is None:
-                log.error(f"config entry {key} missing")
-                log.error("please run: " + sys.argv[0] + " config")
-                raise SystemExit(2)
+            if not value:
+                self.log.error(f"config entry {key} missing")
+                return False
+        self.api = api.Synapse_admin(
+            self.log,
+            self.config["user"], self.config["token"],
+            self.config["base_url"], self.config["admin_path"]
+        )
+        return True
 
     def write_config(self, config):
         try:
-            with open(self.config_yaml, "w") as handle:
-                yaml.dump(data, handle, default_flow_style=False,
-                                 allow_unicode=True)
+            os.makedirs(os.path.dirname(self.config_path), exist_ok=True)
+            with open(self.config_path, "w") as handle:
+                yaml.dump(config, handle, default_flow_style=False,
+                    allow_unicode=True)
         except Exception as error:
-            log.error(" trying to write configuration")
+            self.log.error("{} trying to write configuration".format(error))
 
     def output(self, data):
         click.echo(APIClient.FORMATTERS[self.format](data))
-        
-
-
-def create_config_dir():
-    home = Path(os.getenv('HOME'))
-    Path.mkdir(home / '.config', exist_ok=True)
-    synadm_config = home / '.config'
-    return synadm_config
-
-
-def create_data_dir():
-    home = Path(os.getenv('HOME'))
-    Path.mkdir(home / '.local' / 'share' / 'synadm', parents=True, exist_ok=True)
-    synadm_data = home / '.local' / 'share' / 'synadm'
-    return synadm_data
-
-
-def logger_init():
-    synadm_data = create_data_dir()
-    debug_log = synadm_data / "debug.log"
-
-    log = logging.getLogger('synadm')
-    log.setLevel(logging.DEBUG) # level of logger itself
-    f_handle = logging.FileHandler(debug_log, encoding='utf-8') # create file handler
-    f_handle.setLevel(logging.DEBUG) # which logs even debug messages
-    c_handle = logging.StreamHandler() # console handler with a higher log level
-    c_handle.setLevel(logging.WARNING) # level of the console handler
-    # create formatters and add it to the handlers
-    f_form = logging.Formatter('%(asctime)s %(name)-8s %(levelname)-7s %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S')
-    c_form = logging.Formatter('%(levelname)-5s %(message)s')
-    c_handle.setFormatter(c_form)
-    f_handle.setFormatter(f_form)
-    log.addHandler(c_handle) # add the handlers to logger
-    log.addHandler(f_handle)
-    return log
 
 
 def modify_usage_error(main_command):
@@ -138,10 +133,6 @@ def modify_usage_error(main_command):
     click.exceptions.UsageError.show = show
 
 
-# handle logging and configuration prerequisites
-log = logger_init()
-
-
 @click.group(invoke_without_command=False, context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('--verbose', '-v', count=True, default=False,
       help="enable INFO (-v) or DEBUG (-vv) logging on console")
@@ -151,12 +142,10 @@ log = logger_init()
       help="configuration file path", show_default=True)
 @click.pass_context
 def root(ctx, verbose, output, config_file):
-    if verbose == 1:
-        log.handlers[0].setLevel(logging.INFO)
-    elif verbose > 1:
-        log.handlers[0].setLevel(logging.DEBUG)
-    ctx.obj = APIClient(config_file, output)
-    log.debug("ctx.obj: {}\n".format(ctx.obj))
+    ctx.obj = APIClient(config_file, verbose, output)
+    if ctx.invoked_subcommand != 'config' and not ctx.obj.read_config():
+        click.echo("Please setup synadm: " + sys.argv[0] + " config")
+        raise SystemExit(2)
 
 
 @root.command()
@@ -172,35 +161,30 @@ def root(ctx, verbose, output, config_file):
 @click.option('--admin-api-path', '-p', type=str, default='/_synapse/admin',
     help="""the path Synapse provides its admin API's, usually the default is
     alright for most installations.""", show_default=True)
-@click.option('--view', type=click.Choice(['table', 'raw']), default='raw',
+@click.option('--output', type=click.Choice(['human', 'json', 'yaml', 'pprint']), default='human',
     help="""how should synadm display data by default? 'table' gives a
     tabular view but needs your terminal to be quite width. 'raw' shows
     formatted json exactely as the API responded. Note that this can always
     be overridden by using global switches -r and -t (eg 'synadm -r user
     list')""", show_default=True)
 @click.pass_obj
-def config(api, user, token, base_url, admin_api_path, view):
+def config(api, user, token, base_url, admin_api_path, output):
     """modify synadm's configuration. configuration details are asked
     interactively but can also be provided using command line options."""
     click.echo('Running configurator...')
-    cfg = api.config
-    # get defaults for prompts from either config file or commandline
-    user_default = cfg.user if cfg.user else user
-    token_default = cfg.token if cfg.token else token
-    base_url_default = cfg.base_url if cfg.base_url else base_url
-    admin_path_default = cfg.admin_path if cfg.admin_path else admin_api_path
-    view_default = cfg.view if cfg.view else view
-    # prompts
-    api_user = click.prompt("Synapse admin user name", default=user_default)
-    api_token = click.prompt("Synapse admin user token", default=token_default)
-    api_base_url = click.prompt("Synapse base URL", default=base_url_default)
-    api_admin_path = click.prompt("Synapse admin API path", default=admin_path_default)
-    api_view = click.prompt("How should data be viewed by default?",
-           default=view_default, type=click.Choice(['table', 'raw']))
-
-    conf_dict = {"user": api_user, "token": api_token, "base_url": api_base_url,
-          "admin_api_path": api_admin_path, "view": api_view}
-    cfg.write(conf_dict)
+    api.write_config({
+        "user": click.prompt("Synapse admin user name",
+            default=api.config.get("user", user)),
+        "token": click.prompt("Synapse admin user token",
+            default=api.config.get("token", token)),
+        "base_url": click.prompt("Synapse base URL",
+            default=api.config.get("base_url", base_url)),
+        "admin_api_path": click.prompt("Synapse admin API path",
+            default=api.config.get("admin_api_path", admin_api_path)),
+        "format": click.prompt("How should data be viewed by default?",
+            default=api.config.get("format", output),
+            type=click.Choice(['human', 'json', 'yaml', 'pprint']))
+    })
 
 
 @root.command()
