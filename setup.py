af1445512f55b036c36b52b152d3c3396f4c9547
kaiyou <pierre@jaury.eu>
2020-12-13 13:12:35 +0100

    Update the setup script to handle a package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='synadm',
-    version='0.13',
-    py_modules=['synadm'],
+    version='0.14',
+    packages=find_packages(),
     install_requires=[
         'Click>=7.0,<8.0',
         'requests',
@@ -13,6 +13,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        synadm=synadm:synadm
+        synadm=synadm.cli:root
     ''',
 )
