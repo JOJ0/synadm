b3d3467e722d183dc4ed22b154469945bc6a07ab
J0J0 T <jojo@peek-a-boo.at>
2021-01-28 08:35:43 +0100

    add media quarantine command draft
    leave a FIXME regarding server_name

--- synadm/cli/media.py
+++ synadm/cli/media.py
@@ -4,6 +4,7 @@
 import click
 
 from synadm import cli
+import click_option_group
 
 
 @cli.root.group()
@@ -15,7 +16,7 @@ def media():
 @media.command(name="list")
 @click.argument("room_id", type=str)
 @click.pass_obj
-def media_list_cmd(helper,room_id):
+def media_list_cmd(helper, room_id):
     """ list all media in a room
     """
     media_list = helper.api.room_media_list(room_id)
@@ -23,3 +24,45 @@ def media_list_cmd(helper,room_id):
         click.echo("Media list could not be fetched.")
         raise SystemExit(1)
     helper.output(media_list)
+
+
+@media.command(name="quarantine")
+@click_option_group.optgroup.group(
+    "Quarantine media by",
+    cls=click_option_group.RequiredAnyOptionGroup,
+    help="")
+@click_option_group.optgroup.option(
+    "--media-id", "-i", type=str,
+    help="""the media with this specific media ID will be quarantined.
+    """)
+@click_option_group.optgroup.option(
+    "--room-id", "-r", type=str,
+    help="""all media in room with this room ID (!abcdefg) will be
+    quarantined.""")
+@click_option_group.optgroup.option(
+    "--user-id", "-u", type=str,
+    help="""all media uploaded by user with this matrix ID (@user:server) will
+    be quarantined.""")
+@click.option(
+    "--server-name", "-s", type=str,
+    help="""the local server name, mandatory when --media-id is used. FIXME
+    this should be fetched from somewhere automatically.""")
+@click.pass_obj
+def media_quarantine_cmd(helper, server_name, media_id, user_id, room_id):
+    """ quarantine media in rooms, by users or by media ID
+    """
+    if media_id and not server_name:
+        click.echo("Server name missing.")
+    elif server_name and not media_id:
+        click.echo("Media ID missing.")
+    if media_id and server_name:
+        media_quarantined = helper.api.media_quarantine(server_name, media_id)
+    elif room_id:
+        media_quarantined = helper.api.room_media_quarantine(room_id)
+    elif user_id:
+        media_quarantined = helper.api.user_media_quarantine(user_id)
+
+    if media_quarantined is None:
+        click.echo("Media could not be quarantined.")
+        raise SystemExit(1)
+    helper.output(media_quarantined)
