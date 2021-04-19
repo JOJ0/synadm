b2fa1886e302eea36d89f16c653b91fc8723e90a
J0J0 T <jojo@peek-a-boo.at>
2021-04-19 09:31:35 +0200

    Bump version: 0.28 → 0.29

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md') as f:
 
 setup(
     name="synadm",
-    version="0.28",
+    version="0.29",
     author="Johannes Tiefenbacher",
     author_email="jt@peek-a-boo.at",
     description="Command line admin tool for Synapse (Matrix reference homeserver)",
