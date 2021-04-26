3ce7f234f634d4d22a4611f018a84891a36d4931
J0J0 T <jojo@peek-a-boo.at>
2021-04-26 08:46:28 +0200

    sphinx conf: final settings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -68,8 +68,8 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
-    'collapse_navigation': False,
-    'navigation_depth': 1,
+    'collapse_navigation': True,
+    'navigation_depth': 8,
     'sticky_navigation': False,
     'prev_next_buttons_location': 'both'
 }
