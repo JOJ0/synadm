a57ce17f1559989ec88c512f73fab61dc0d77528
J0J0 T <jojo@peek-a-boo.at>
2021-09-14 07:44:50 +0200

    media quarantine: retrieve local server name via API

--- synadm/cli/media.py
+++ synadm/cli/media.py
@@ -102,15 +102,18 @@ def media_list_cmd(ctx, helper, room_id, user_id, from_, limit, sort, reverse):
     be quarantined.""")
 @click.option(
     "--server-name", "-s", type=str,
-    help="""the server name of the media, mandatory when --media-id is used.
+    help="""the server name of the media, mandatory when --media-id is used and
+    _remote_ media should be processed. For locally stored media this option can
+    be omitted.
     """)
 @click.pass_obj
 def media_quarantine_cmd(helper, server_name, media_id, user_id, room_id):
     """ quarantine media in rooms, by users or by media ID
     """
     if media_id and not server_name:
-        click.echo("Server name missing.")
-        media_quarantined = None
+        # We assume it is local media and fetch our own server name.
+        fetched_name = helper.matrix_api.server_name()
+        media_quarantined = helper.api.media_quarantine(fetched_name, media_id)
     elif server_name and not media_id:
         click.echo("Media ID missing.")
         media_quarantined = None
