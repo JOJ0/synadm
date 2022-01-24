e121b87bba9ced4ca8870b4229449e0eb08e3635
J0J0 T <jojo@peek-a-boo.at>
2022-01-24 23:26:06 +0100

    setup.py: Add Python 3.10

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
         "Topic :: System :: Systems Administration",
         "Topic :: Communications :: Chat",
