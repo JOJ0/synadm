0d2a1f72bc67454c776f7465e51c44849d4786a8
Jackson Chen <jackson@jacksonchen666.com>
2023-09-08 19:02:39 +0200

    Bump version: 0.42 → 0.43

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,10 +22,10 @@ copyright = '2020-2023, Johannes Tiefenbacher'
 author = 'Johannes Tiefenbacher'
 
 # The short X.Y version
-version = '0.42'
+version = '0.43'
 
 # The full version, including alpha/beta/rc tags
-release = '0.42'
+release = '0.43'
 
 
 # -- General configuration ---------------------------------------------------
