d193708d68085e1c95cde951ffd6725dd46895d5
Nicolas Peugnet <n.peugnet@free.fr>
2023-11-29 14:39:03 +0100

    Apply suggestions from code review
    Co-authored-by: J0J0 Todos <2733783+JOJ0@users.noreply.github.com>

--- synadm/cli/__init__.py
+++ synadm/cli/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" CLI base functions and settings
+""" CLI root-level commands; Subcommands are imported at the bottom of file
 """
 
 import sys
--- synadm/cli/_helper.py
+++ synadm/cli/_helper.py
@@ -15,8 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" API helper, exported in its own file to allow importing it only
-when needed."""
+""" CLI helpers and utilities"""
 
 import os
 import logging
