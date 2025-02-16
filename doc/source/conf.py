9e3cc27793e571fb3901996c6e92802d157e1dc4
J0J0 Todos <jojo@peek-a-boo.at>
2025-02-16 16:16:26 +0100

    Bump version: 0.46.1 → 0.47

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,10 +22,10 @@ copyright = '2020-2024, Johannes Tiefenbacher'
 author = 'Johannes Tiefenbacher'
 
 # The short X.Y version
-version = '0.46.1'
+version = '0.47'
 
 # The full version, including alpha/beta/rc tags
-release = '0.46.1'
+release = '0.47'
 
 
 # -- General configuration ---------------------------------------------------
